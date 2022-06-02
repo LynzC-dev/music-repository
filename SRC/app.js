0704c3144f18c5530278f69bbdbcae5ea1bc05af
Lyndsey Coombs <lyndscoombs@aol.com>
2022-06-02 11:34:19 +0100

    first test

--- SRC/app.js
+++ SRC/app.js
@@ -1,10 +1,11 @@
+
 const express = require('express');
+const artistRouter = require('./routes/artist');
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.status(200).send( {result: "Hello, World"})
-})
+app.use('/artist', artistRouter);
 
 module.exports = app;
--- utils/drop-database.js
+++ utils/drop-database.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const path = require('path');
 
 require('dotenv').config({
