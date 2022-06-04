f197d7dacb70beeb97c36d404eec1c35d6a47a20
Lyndsey Coombs <lyndscoombs@aol.com>
2022-06-04 07:09:17 +0100

    delete controller add

--- SRC/controllers/artist.js
+++ SRC/controllers/artist.js
@@ -16,6 +16,78 @@ exports.create = async (req, res) => {
     } catch (err) {
       res.sendStatus(500).json(err);
     }
-  
     db.close();
-  };
+};
+
+    exports.read = async (_, res) => {
+        const db = await getDb();
+      
+        try {
+          const [artists] = await db.query('SELECT * FROM Artist');
+      
+          res.status(200).json(artists);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+        db.close();
+      };
+
+      exports.readById = async (req, res) => {
+        const db = await getDb();
+        const { artistId } = req.params;
+      
+        const [[artist]] = await db.query('SELECT * FROM Artist WHERE id = ?', [
+          artistId,
+        ]);
+      
+        if (!artist) {
+          res.sendStatus(404);
+        } else {
+          res.status(200).json(artist);
+        }
+      
+        db.close();
+      };
+
+      exports.update = async(req, res) => {
+        const db = await getDb();
+        const data = req.body;
+        const { artistId } = req.params;
+
+        try {
+          const [
+            { affectedRows },
+          ] = await db.query('UPDATE Artist SET ? WHERE id = ?', [data, artistId]);
+      
+          if (!affectedRows) {
+            res.sendStatus(404);
+          } else {
+            res.status(200).send();
+          }
+        } catch (err) {
+          res.sendStatus(500);
+        }
+      
+        db.close();
+      };
+
+      exports.destroy = async(req, res) => {
+        const db = await getDb();
+        const {artistId} = req.params;
+
+        try {
+          const [
+            { affectedRows },
+          ] = await db.query('DELETE FROM Artist WHERE id = ?', [ artistId]);
+      
+          if (!affectedRows) {
+            res.sendStatus(404);
+          } else {
+            res.status(200).send();
+          }
+        } catch (err) {
+          res.sendStatus(500);
+        }
+      
+        db.close();
+      };
--- SRC/routes/artist.js
+++ SRC/routes/artist.js
@@ -4,5 +4,9 @@ const artistController = require('../controllers/artist');
 const router = express.Router();
 
 router.post('/', artistController.create);
+router.get('/', artistController.read);
+router.get('/:artistId', artistController.readById);
+router.patch('/:artistId', artistController.update);
+router.delete('/:artistId', artistController.destroy);
 
 module.exports = router;
